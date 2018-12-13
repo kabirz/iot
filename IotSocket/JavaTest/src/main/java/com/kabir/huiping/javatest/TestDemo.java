9f2ba525f337acd5af298289d29095657d3cd7b3
kabir <jxwazxzhp@126.com>
2018-12-13 11:24:07 +0800

    update r1

--- IotSocket/JavaTest/src/main/java/com/kabir/huiping/javatest/TestDemo.java
+++ IotSocket/JavaTest/src/main/java/com/kabir/huiping/javatest/TestDemo.java
@@ -3,6 +3,6 @@ package com.kabir.huiping.javatest;
 public class TestDemo {
     public static void main(String[] args) {
 //        Hello.printHello();
-        System.out.println(myTool.print9(9 + 2));
+        System.out.println(myTool.print9(9));
     }
 }
--- IotSocket/app/src/main/java/com/kabir/huiping/iotsocket/MainActivity.java
+++ IotSocket/app/src/main/java/com/kabir/huiping/iotsocket/MainActivity.java
@@ -1,15 +1,13 @@
 package com.kabir.huiping.iotsocket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
-    private Button button;
     private int count;
 
     @Override
@@ -17,14 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
         count = 0;
 
     }
 
     public void onButtonClick(View view) {
-        count++;
-        editText.setText("count is " + count);
+        String info = (String) getResources().getText(R.string.test);
+        info = info + " " + count++;
+        editText.setText(info);
     }
 
 }
